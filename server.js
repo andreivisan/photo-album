b1999b7458ae82c2c1b636cd8de77c1aff7ed3b7
andreivisan <andrei.m.visan@gmail.com>
2022-03-17 09:16:36 +0100

    Added environment variables support

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const folderRoutes = require('./routes/folderRoutes')
 
+require('dotenv').config();
+
 const app = express();
 
+const PORT = 3000
+
 // listen for requests
-app.listen(3000);
+app.listen(PORT, ()=>{
+  console.log(`Server Running on port ${PORT}`);
+ });
 
 var corsOptions = {
   origin: "http://localhost:8081"
