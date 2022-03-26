8346b35db1746d47b69df2e73560095030367f7b
andreivisan <andrei.m.visan@gmail.com>
2022-03-26 16:39:41 +0100

    Finished Photo API

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -24,6 +24,76 @@ const photo_create_post = async (req, res) => {
     }
 }
 
+const photo_index = async (req, res) => {
+    try {
+       const photos = await Photo.findAll();
+       
+       return res.json(photos)
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json(error);
+    }
+}
+
+const photo_details = async (req, res) => {
+    const uuid = req.params.uuid;
+    try {
+        const photo = await Photo.findOne({
+            where: { uuid },
+        });
+
+        return res.json(photo);
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json(error);
+    }
+}
+
+const photo_delete = async (req, res) => {
+    const uuid = req.params.uuid;
+    try {
+        const photo = await Photo.findOne({ where: { uuid } });
+
+        await photo.destroy();
+
+        return res.json({ message: 'Folder deleted!' })
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json(error);
+    }
+}
+
+const photo_update = async (req, res) => {
+    const uuid = req.params.uuid;
+    const { name, path, camera, file_size, resolution, notes } = req.body;
+
+    try {
+        const photo = await Photo.findOne({ where: { uuid } });
+
+        photo.name = name;
+        photo.path = path;
+        photo.camera = camera;
+        photo.file_size = file_size;
+        photo.resolution = resolution;
+        photo.notes = notes;
+
+        await photo.save();
+        
+        return res.json(photo);
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json(error);
+    }
+}
+
 module.exports = {
     photo_create_post,
+    photo_index,
+    photo_details,
+    photo_delete,
+    photo_update
 }
