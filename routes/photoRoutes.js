d9b9487c87cb3271bdaf85cfc1045b46822c371b
andreivisan <andrei.m.visan@gmail.com>
2022-03-26 16:39:41 +0100

    Finished Photo API

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -4,5 +4,9 @@ const photoController = require('../controllers/photoController')
 const router = express.Router();
 
 router.post('/', photoController.photo_create_post);
+router.get('/', photoController.photo_index);
+router.get('/:uuid', photoController.photo_details);
+router.delete('/:uuid', photoController.photo_delete);
+router.put('/:uuid', photoController.photo_update);
 
 module.exports = router
