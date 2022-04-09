505c0ba8cb30e77fca337b0c8cbf312739d3c16e
andreivisan <andrei.m.visan@gmail.com>
2022-04-09 20:29:53 +0200

    Added Vue app + TailwindCSS integration + Header Component

---
+++ app/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="grid grid-cols-9 gap-4 mt-6">
+        <div class="col-span-4 text-left ml-6">
+            <a href="" class="py-2 px-6 text-black border-b-2 border-black font-semibold ">Albums</a>
+            <a href="" class="py-2 px-6 text-black font-semibold hover:text-gray-500 transition duration-300">All Photos</a>
+        </div>
+        <div class="bg-black text-white font-bold italic text-xl pt-1">
+            <p>MY PHOTO ALBUM</p>
+        </div>
+        <div class="col-span-4 text-right mr-6">
+            <div class="relative text-white focus-within:text-gray-400">
+                <span class="absolute inset-y-0">
+                    <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
+                        <svg fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    </button>
+                </span>
+                <input type="search" name="q" class="py-2 text-sm text-white bg-black rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autocomplete="off">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
+    <Header />
   </div>
 </template>
 
 <script>
+  import Header from '../components/Header.vue'
 
-export default {
-  name: 'Home',
-  components: { }
-}
+  export default {
+    name: 'Home',
+    components: { Header }
+  }
 </script>
