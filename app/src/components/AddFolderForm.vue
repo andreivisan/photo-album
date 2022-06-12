8d8725d117a058ba307585a5fe9a8c662e7faec7
andreivisan <andrei.m.visan@gmail.com>
2022-06-12 17:56:30 +0200

    Refresh folder list after submit

--- app/src/components/AddFolderForm.vue
+++ app/src/components/AddFolderForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="bg-white px-8 pt-6 pb-8 mb-4" @submit.prevent="handleSaveFolder">
+  <form class="bg-white px-8 pt-6 pb-8 mb-4" @submit="handleSaveFolder">
     <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="folderName">
             Folder Name
